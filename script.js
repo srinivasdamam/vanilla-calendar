8b32049fd19172eef51927bc93d32c2cb807a5e5
Srinivas Damam <srinivasdamam@gmail.com>
2023-07-20 08:53:34 +0530

    Display dates in the calendar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let nav = 0;
 let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
+const calendar = document.getElementById('calendar');
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
@@ -12,11 +13,36 @@ function load() {
     const date = dt.getDate();
     const year = dt.getFullYear();
 
+    const firstDayOfMonth = new Date(year, month, 1);
     // To identify days in the month, we need to initiate new date
     // and pass the current month plus 1 and pass 0 as 3rd argument
     // 3rd argument is day number, 0 means pervious day 
     const daysInMonth = new Date(year, month+1, 0).getDate();
-    console.log("Days in current month: ", daysInMonth);
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-IN', {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+
+    });
+    
+    const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
+    
+    for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+            daySquare.addEventListener('click', () => {
+                console.log('clicked');
+            })
+        } else {
+            daySquare.classList.add('padding');
+        }
+        calendar.appendChild(daySquare);
+
+    }
 
 }
 
